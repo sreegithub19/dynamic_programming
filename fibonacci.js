25c781fa54cce8f944efc6932dc646d2e58cf3e5
K <sreedhar.k@rakuten.com>
2022-02-14 09:48:48 +0530

    Added code

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+// Maximum Fibonacci in nodejs
+// Command to run : node  --stack-size=20000 fibonacci.js
+
+const fib = (n, memo = {}) => {
+    if(n in memo) return memo[n];
+    if(n<=(2n)) return BigInt(1);
+    memo[n] = (fib(n-1n,memo)) + (fib(n-2n,memo));
+    return memo[n];
+}
+
+console.log(fib(BigInt(61626)));
+console.log(fib(BigInt(61626)).toString().length);   // 12879
+
+console.log(Number.MAX_SAFE_INTEGER);   // 9007199254740991
+
+// Beyond the next line's BigInt value is also possible, but takes a long time to get the value
+         //console.log(((BigInt(Number.MAX_VALUE) + BigInt(Math.pow(Number.MAX_SAFE_INTEGER,19))) ** 10000n))
+//.toString()
+//.length);
